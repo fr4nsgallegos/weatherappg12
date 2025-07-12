4632b5061e3743731002a5acfd39025ef0e59558
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-07-11 21:49:55 -0500

    traduciendo el json a model

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:weatherappg12/models/weather_model.dart';
 
 class ApiServices {
-  Future<void> getWeatherInfo() async {
+  Future<WeatherModel?> getWeatherInfo() async {
     final url = Uri.parse(
       "http://api.weatherapi.com/v1/current.json?key=70866d7ade244a3c9ca20142230509&q=Lima&aqi=no",
     );
@@ -15,9 +16,11 @@ class ApiServices {
       print(response.statusCode);
       print(response.body);
       print("----------------------");
-
       final data = json.decode(response.body);
-      print(data["location"]["name"]);
+      WeatherModel weatherModel = WeatherModel.fromJson(data);
+      print(weatherModel);
+      print(weatherModel.location.name);
+      return weatherModel;
     } else {
       throw Exception("error al cargar: ${response.statusCode}");
     }
